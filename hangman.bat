8e9f67fc3ed95c89dffffcd7cb5012fefd113ca5
notdeckvrd <73615640+notdeckvrd@users.noreply.github.com>
2021-01-26 09:17:38 -0500

    feat: user input
    Store if the user wants to play into a variable.

--- hangman.bat
+++ hangman.bat
@@ -12,5 +12,18 @@ echo    *    *  *    *  *    *  ******  *     *  *    *  *    *
 echo.
 echo ============================================================
 echo.
-echo Wanna Play? (y/n)
+set /P _confirm= Wanna Play? (y/n):
+if %_confirm% == y goto :play
+if %_confirm% == n goto :noplay
+:play
+echo.
+echo Lets play!
+goto :startgame
+exit /B 0
+:noplay
+echo That is unfortunate. Hope to see you soon.
+exit /B 0
+:startgame
+echo But first, let us go over the rules.
+exit /B 0
 pause>nil
